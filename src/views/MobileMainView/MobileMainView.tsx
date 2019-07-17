0040f7e4846a43de2f3d6c77ccf0289d93606403
SkalskiP <piotr.skalski92@gmail.com>
2019-07-17 22:36:18 +0200

    Mobile view in progress

--- src/views/MobileMainView/MobileMainView.tsx
+++ src/views/MobileMainView/MobileMainView.tsx
@@ -10,20 +10,42 @@ interface IProps {
 }
 
 const MobileMainView: React.FC<IProps> = ({size}) => {
+
+    const topNavigationBar = <div className="MobileTopNavigationBar">
+        <div className="NavigationBarGroupWrapper">
+            <div className="Header">
+                <img alt={"make-sense"} src={"/make-sense-ico-transparent.png"}/>
+                Make Sense
+            </div>
+        </div>
+        <div className="TriangleHorizontal Bottom">
+            <div className="TriangleHorizontalContent"/>
+        </div>
+    </div>;
+
+    const firstStage = <div className="FirstStage"/>;
+    const secondStage = <div className="SecondStage"/>;
+    const thirdStage = <div className="ThirdStage">
+        <div className="TriangleHorizontal Top">
+            <div className="TriangleHorizontalContent"/>
+        </div>
+        <div className="TriangleHorizontal Bottom">
+            <div className="TriangleHorizontalContent"/>
+        </div>
+    </div>;
+    const fourthStage = <div className="FourthStage"/>;
+
     return(<div className="MobileMainView">
+        {topNavigationBar}
         <Scrollbars>
             <div
                 className="MobileMainViewContent"
                 style={{width: size.width}}
             >
-                <div className="TopRow">
-                    <div className="TriangleHorizontal">
-                        <div className="TriangleHorizontalContent"/>
-                    </div>
-                </div>
-                <div className="BottomRow">
-
-                </div>
+                {/*{firstStage}*/}
+                {secondStage}
+                {thirdStage}
+                {fourthStage}
             </div>
         </Scrollbars>
     </div>)
