5c4329d4f7cbc16e8623970d81f2d7d25c7ae8df
SkalskiP <piotr.skalski92@gmail.com>
2019-07-31 23:09:58 +0200

    Please resize view

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,29 @@ import {connect} from "react-redux";
 import PopupView from "./views/PopupView/PopupView";
 import {MobileDeviceData} from "./data/MobileDeviceData";
 import MobileMainView from "./views/MobileMainView/MobileMainView";
+import {ISize} from "./interfaces/ISize";
+import {Settings} from "./settings/Settings";
+import {SizeItUpView} from "./views/SizeItUpView/SizeItUpView";
 
 interface IProps {
     projectType: ProjectType;
     mobileDeviceData: MobileDeviceData;
+    windowSize: ISize;
 }
 
-const App: React.FC<IProps> = ({projectType, mobileDeviceData}) => {
+const App: React.FC<IProps> = ({projectType, mobileDeviceData, windowSize}) => {
     const selectRoute = () => {
         if (!!mobileDeviceData.manufacturer && !!mobileDeviceData.os)
             return <MobileMainView/>;
         if (!projectType)
             return <MainView/>;
-        else
-            return <EditorView/>;
+        else {
+            if (windowSize.height < Settings.EDITOR_MIN_HEIGHT || windowSize.width < Settings.EDITOR_MIN_WIDTH) {
+                return <SizeItUpView/>;
+            } else {
+                return <EditorView/>;
+            }
+        }
     };
 
       return (
@@ -34,7 +43,8 @@ const App: React.FC<IProps> = ({projectType, mobileDeviceData}) => {
 
 const mapStateToProps = (state: AppState) => ({
     projectType: state.editor.projectType,
-    mobileDeviceData: state.general.mobileDeviceData
+    mobileDeviceData: state.general.mobileDeviceData,
+    windowSize: state.general.windowSize
 });
 
 export default connect(
--- src/views/EditorView/FeatureInProgress/FeatureInProgress.tsx
+++ src/views/EditorView/FeatureInProgress/FeatureInProgress.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import './FeatureInProgress.scss';
 
 export const FeatureInProgress: React.FC = () => {
-    return(<div className="FeatureInProgress">
-        <p className="extraBold">Coming soon...</p>
-        <img
-            draggable={false}
-            alt={"fingers_crossed"}
-            src={"img/fingers_crossed.png"}
-        />
-        <p>Keep your fingers crossed!</p>
-    </div>)
+    return(
+        <div
+            className="FeatureInProgress"
+        >
+            <p className="extraBold">Coming soon...</p>
+            <img
+                draggable={false}
+                alt={"fingers_crossed"}
+                src={"img/fingers_crossed.png"}
+            />
+            <p>Keep your fingers crossed!</p>
+        </div>
+    )
 };
