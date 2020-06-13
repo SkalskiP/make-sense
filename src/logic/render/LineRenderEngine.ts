7d28471e0729c90ac94c9d3ff8189046ccd3c551
SkalskiP <piotr.skalski92@gmail.com>
2020-06-13 14:11:22 +0200

    line rendering engine is working

--- src/logic/render/LineRenderEngine.ts
+++ src/logic/render/LineRenderEngine.ts
@@ -17,11 +17,13 @@ import {EditorActions} from "../actions/EditorActions";
 import {LabelsSelector} from "../../store/selectors/LabelsSelector";
 import {DrawUtil} from "../../utils/DrawUtil";
 import {GeneralSelector} from "../../store/selectors/GeneralSelector";
-import {CanvasUtil} from "../../utils/CanvasUtil";
 import uuidv1 from "uuid/v1";
 import {ILine} from "../../interfaces/ILine";
 import {LineUtil} from "../../utils/LineUtil";
 import {IRect} from "../../interfaces/IRect";
+import {updateCustomCursorStyle} from "../../store/general/actionCreators";
+import {CustomCursorStyle} from "../../data/enums/CustomCursorStyle";
+import {LineAnchorType} from "../../data/enums/LineAnchorType";
 
 export class LineRenderEngine extends BaseRenderEngine {
     private config: RenderEngineConfig = new RenderEngineConfig();
@@ -30,7 +32,8 @@ export class LineRenderEngine extends BaseRenderEngine {
     // STATE
     // =================================================================================================================
 
-    private startLinePoint: IPoint;
+    private lineCreationStartPoint: IPoint;
+    private lineUpdateAnchorType: LineAnchorType;
 
     public constructor(canvas: HTMLCanvasElement) {
         super(canvas);
@@ -46,32 +49,38 @@ export class LineRenderEngine extends BaseRenderEngine {
         const isMouseOverCanvas: boolean = RenderEngineUtil.isMouseOverCanvas(data);
 
         if (isMouseOverCanvas) {
-            if (!this.isInProgress()) {
+            const anchorTypeUnderMouse = this.getAnchorTypeUnderMouse(data);
+            if (!!anchorTypeUnderMouse && !this.isResizeInProgress()) {
+                const labelLine: LabelLine = this.getLineUnderMouse(data);
+                this.startExistingLabelResize(labelLine.id, anchorTypeUnderMouse)
+            } else if (!this.isInProgress()) {
                 if (isMouseOverImage) {
-                    this.startLinePoint = RenderEngineUtil.setPointBetweenPixels(data.mousePositionOnViewPortContent)
+                    this.lineCreationStartPoint = RenderEngineUtil.setPointBetweenPixels(data.mousePositionOnViewPortContent)
                     EditorActions.setViewPortActionsDisabledStatus(true);
                 }
             } else {
                 const mousePositionSnapped: IPoint = RectUtil.snapPointToRect(
                     data.mousePositionOnViewPortContent, data.viewPortContentImageRect
                 );
-                const line = {start: this.startLinePoint, end: mousePositionSnapped}
+                const line = {start: this.lineCreationStartPoint, end: mousePositionSnapped}
                 const lineOnImage = RenderEngineUtil.transferLineFromViewPortContentToImage(line, data)
                 this.addLineLabel(lineOnImage)
-                this.startLinePoint = null
+                this.lineCreationStartPoint = null
                 EditorActions.setViewPortActionsDisabledStatus(false);
             }
         }
     }
 
     public mouseUpHandler(data: EditorData): void {
-        console.log("MOUSE UP")
+        if (this.isResizeInProgress()) {
+            this.endExistingLabelResize(data)
+        }
     }
 
     public mouseMoveHandler(data: EditorData): void {
         const isOverImage: boolean = RenderEngineUtil.isMouseOverImage(data);
         if (isOverImage) {
-            const labelLine: LabelLine = this.getLineUnderMouse(data)
+            const labelLine: LabelLine = this.getLineUnderMouse(data);
             if (!!labelLine) {
                 if (LabelsSelector.getHighlightedLabelId() !== labelLine.id) {
                     store.dispatch(updateHighlightedLabelId(labelLine.id))
@@ -91,6 +100,7 @@ export class LineRenderEngine extends BaseRenderEngine {
     public render(data: EditorData): void {
         this.drawExistingLabels(data);
         this.drawActivelyCreatedLabel(data)
+        this.drawActivelyResizeLabel(data)
         this.updateCursorStyle(data);
     }
 
@@ -101,24 +111,45 @@ export class LineRenderEngine extends BaseRenderEngine {
         imageData.labelLines.forEach((labelLine: LabelLine) => {
             const isActive: boolean = labelLine.id === activeLabelId || labelLine.id === highlightedLabelId;
             const lineOnCanvas = RenderEngineUtil.transferLineFromImageToViewPortContent(labelLine.line, data)
-            this.drawLine(lineOnCanvas, isActive)
+            if (!(labelLine.id === activeLabelId && this.isResizeInProgress())) {
+                this.drawLine(lineOnCanvas, isActive)
+            }
         });
     }
 
     private drawActivelyCreatedLabel(data: EditorData) {
         if (this.isInProgress()) {
-            const line = {start: this.startLinePoint, end: data.mousePositionOnViewPortContent}
+            const line = {start: this.lineCreationStartPoint, end: data.mousePositionOnViewPortContent}
             DrawUtil.drawLine(this.canvas, line.start, line.end, this.config.lineActiveColor, this.config.lineThickness);
+            const lineStartHandle = RectUtil.getRectWithCenterAndSize(this.lineCreationStartPoint, this.config.anchorSize);
+            DrawUtil.drawRectWithFill(this.canvas, lineStartHandle, this.config.activeAnchorColor);
+        }
+    }
+
+    private drawActivelyResizeLabel(data: EditorData) {
+        const activeLabelLine: LabelLine = LabelsSelector.getActiveLineLabel();
+        if (!!activeLabelLine && this.isResizeInProgress()) {
+            const snappedMousePosition: IPoint =
+                RectUtil.snapPointToRect(data.mousePositionOnViewPortContent, data.viewPortContentImageRect);
+            const lineOnCanvas = RenderEngineUtil.transferLineFromImageToViewPortContent(activeLabelLine.line, data)
+            const lineToDraw = {
+                start: this.lineUpdateAnchorType === LineAnchorType.START ? snappedMousePosition : lineOnCanvas.start,
+                end: this.lineUpdateAnchorType === LineAnchorType.END ? snappedMousePosition : lineOnCanvas.end
+            }
+            this.drawLine(lineToDraw, true)
         }
     }
 
     private updateCursorStyle(data: EditorData) {
         if (!!this.canvas && !!data.mousePositionOnViewPortContent && !GeneralSelector.getImageDragModeStatus()) {
-            if (RectUtil.isPointInside(
-                {x: 0, y: 0, ...CanvasUtil.getSize(this.canvas)},
-                data.mousePositionOnViewPortContent)
-            ) {
-                RenderEngineUtil.wrapDefaultCursorStyleInCancel(data);
+            const isMouseOverCanvas: boolean = RenderEngineUtil.isMouseOverCanvas(data);
+            if (isMouseOverCanvas) {
+                const anchorTypeUnderMouse = this.getAnchorTypeUnderMouse(data);
+                if (!this.isInProgress() && !!anchorTypeUnderMouse) {
+                    store.dispatch(updateCustomCursorStyle(CustomCursorStyle.MOVE));
+                } else {
+                    RenderEngineUtil.wrapDefaultCursorStyleInCancel(data);
+                }
                 this.canvas.style.cursor = "none";
             } else {
                 this.canvas.style.cursor = "default";
@@ -148,7 +179,16 @@ export class LineRenderEngine extends BaseRenderEngine {
     // =================================================================================================================
 
     public isInProgress(): boolean {
-        return !!this.startLinePoint
+        return !!this.lineCreationStartPoint
+    }
+
+    public isResizeInProgress(): boolean {
+        return !!this.lineUpdateAnchorType;
+    }
+
+    private isMouseOverAnchor(mouse: IPoint, anchor: IPoint): boolean {
+        if (!mouse || !anchor) return null;
+        return RectUtil.isPointInside(RectUtil.getRectWithCenterAndSize(anchor, this.config.anchorSize), mouse);
     }
 
     // =================================================================================================================
@@ -170,6 +210,48 @@ export class LineRenderEngine extends BaseRenderEngine {
     };
 
     // =================================================================================================================
+    // UPDATE
+    // =================================================================================================================
+
+    private startExistingLabelResize(labelId: string, anchorType: LineAnchorType) {
+        store.dispatch(updateActiveLabelId(labelId));
+        this.lineUpdateAnchorType = anchorType;
+        EditorActions.setViewPortActionsDisabledStatus(true);
+    }
+
+    private endExistingLabelResize(data: EditorData) {
+        this.applyUpdateToLineLabel(data);
+        this.lineUpdateAnchorType = null;
+        EditorActions.setViewPortActionsDisabledStatus(false);
+    }
+
+    private applyUpdateToLineLabel(data: EditorData) {
+        const imageData: ImageData = LabelsSelector.getActiveImageData();
+        const activeLabel: LabelLine = LabelsSelector.getActiveLineLabel();
+        imageData.labelLines = imageData.labelLines.map((lineLabel: LabelLine) => {
+            if (lineLabel.id !== activeLabel.id) {
+                return lineLabel
+            } else {
+                const snappedMousePosition: IPoint =
+                    RectUtil.snapPointToRect(data.mousePositionOnViewPortContent, data.viewPortContentImageRect);
+                const mousePositionOnImage = RenderEngineUtil.transferPointFromViewPortContentToImage(
+                    snappedMousePosition, data
+                );
+                return {
+                    ...lineLabel,
+                    line: {
+                        start: this.lineUpdateAnchorType === LineAnchorType.START ? mousePositionOnImage : lineLabel.line.start,
+                        end: this.lineUpdateAnchorType === LineAnchorType.END ? mousePositionOnImage : lineLabel.line.end
+                    }
+                }
+            }
+        });
+
+        store.dispatch(updateImageDataById(imageData.id, imageData));
+        store.dispatch(updateActiveLabelId(activeLabel.id));
+    }
+
+    // =================================================================================================================
     // GETTERS
     // =================================================================================================================
 
@@ -183,6 +265,20 @@ export class LineRenderEngine extends BaseRenderEngine {
                 this.config.anchorHoverSize.width / 2
             )
             if (mouseOverLine) return labelLines[i]
+        }
+        return null;
+    }
+
+    private getAnchorTypeUnderMouse(data: EditorData): LineAnchorType {
+        const labelLines: LabelLine[] = LabelsSelector.getActiveImageData().labelLines;
+        for (let i = 0; i < labelLines.length; i++) {
+            const lineOnCanvas: ILine = RenderEngineUtil.transferLineFromImageToViewPortContent(labelLines[i].line, data);
+            if (this.isMouseOverAnchor(data.mousePositionOnViewPortContent, lineOnCanvas.start)) {
+                return LineAnchorType.START
+            }
+            if (this.isMouseOverAnchor(data.mousePositionOnViewPortContent, lineOnCanvas.end)) {
+                return LineAnchorType.END
+            }
         }
         return null;
     }
