0b4fc58953fb8e520fe2e53c5d487925565d6c4b
SkalskiP <piotr.skalski92@gmail.com>
2020-09-04 18:16:16 +0200

    fix

--- src/views/EditorView/Editor/Editor.tsx
+++ src/views/EditorView/Editor/Editor.tsx
@@ -173,6 +173,7 @@ class Editor extends React.Component<IProps, IState> {
             else if ((PlatformModel.isMac && scrollSign === 1) || (!PlatformModel.isMac && scrollSign === -1)) {
                 ViewPortActions.zoomIn();
             }
+            EditorModel.mousePositionOnViewPortContent = CanvasUtil.getMousePositionOnCanvasFromEvent(event, EditorModel.canvas);
         }
     };
 
