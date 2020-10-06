e79e6ed5e49744dcacea516029ca81dbbd91fcb6
SkalskiP <piotr.skalski92@gmail.com>
2020-10-06 18:05:30 +0200

    up

--- src/data/export/TagExportFormatData.ts
+++
@@ -1,9 +0,0 @@
-import {IExportFormat} from "../../interfaces/IExportFormat";
-import {ExportFormatType} from "../enums/ExportFormatType";
-
-export const TagExportFormatData: IExportFormat[] = [
-    {
-        type: ExportFormatType.CSV,
-        label: "Single CSV file."
-    }
-];
