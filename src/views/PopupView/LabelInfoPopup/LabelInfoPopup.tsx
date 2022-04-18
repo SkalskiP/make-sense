af64cbda5529ae429f19f82999f0f66d44d97c23
최명진 <mj@dasomx.com>
2022-04-18 13:22:09 +0900

    tag button

--- src/views/PopupView/LabelInfoPopup/LabelInfoPopup.tsx
+++ src/views/PopupView/LabelInfoPopup/LabelInfoPopup.tsx
@@ -7,6 +7,10 @@ import {ImageData} from '../../../store/labels/types';
 import {GenericYesNoPopup} from '../GenericYesNoPopup/GenericYesNoPopup';
 import {updateActivePopupType} from '../../../store/general/actionCreators';
 import {PopupWindowType} from '../../../data/enums/PopupWindowType';
+import {LabelsSelector} from '../../../store/selectors/LabelsSelector';
+import {TagButton} from '../../Common/TagButton/TagButton';
+import {FASHION_STYLE_MAN} from '../../../data/enums/ItemType';
+import {LabelModeType} from '../../../data/enums/LabelType';
 
 interface IProps {
     labelRectId: string;
@@ -19,13 +23,86 @@ const LabelInfoPopup: React.FC<IProps> = ({
     imageData,
     updateActivePopupTypeAction
 }) => {
+    const labelRect = LabelsSelector.getActiveRectLabel();
     const renderContent = () => {
-        return <div className="LabelInfoPopupContent">{labelRectId}</div>;
+        return labelRect.mode === LabelModeType.HUMAN ? (
+            <div className="LabelInfoPopupContent">
+                <div className="AttributeContainer">
+                    <div className="AttributeName">Gender</div>
+                    <div className="AttributeSelector">
+                        <TagButton label="Male" />
+                        <TagButton label="Female" />
+                    </div>
+                </div>
+                <div className="AttributeContainer">
+                    <div className="AttributeName">Human type</div>
+                    <div className="AttributeSelector">
+                        <TagButton label="Human" />
+                        <TagButton label="Mannequin" />
+                        <TagButton label="Stylebook" />
+                    </div>
+                </div>
+                <div className="AttributeContainer">
+                    <div className="AttributeName">Style</div>
+                    <div className="AttributeSelector">
+                        <TagButton label={FASHION_STYLE_MAN.GENTLEMAN} />
+                        <TagButton label={FASHION_STYLE_MAN.SPORTY} />
+                        <TagButton label={FASHION_STYLE_MAN.PUNK} />
+                    </div>
+                </div>
+            </div>
+        ) : (
+            <div className="LabelInfoPopupContent">
+                <div className="AttributeContainer">
+                    <div className="AttributeName">Human ID</div>
+                    <div className="AttributeSelector">
+                        {imageData.humans.map((human, idx) => (
+                            <TagButton
+                                key={String(human.id)}
+                                label={String(idx)}
+                            />
+                        ))}
+                    </div>
+                </div>
+                <div className="AttributeContainer">
+                    <div className="AttributeName">Gender</div>
+                    <div className="AttributeSelector">
+                        <TagButton label="Male" isActive={false} />
+                        <TagButton label="Female" isActive={false} />
+                    </div>
+                </div>
+                <div className="AttributeContainer">
+                    <div className="AttributeName">Main Category</div>
+                    <div className="AttributeSelector"></div>
+                </div>
+                <div className="AttributeContainer">
+                    <div className="AttributeName">Sub Category</div>
+                    <div className="AttributeSelector"></div>
+                </div>
+                <div className="AttributeContainer">
+                    <div className="AttributeName">Item ID</div>
+                    <div className="AttributeSelector"></div>
+                </div>
+                <div className="AttributeContainer">
+                    <div className="AttributeName">Color</div>
+                    <div className="AttributeSelector"></div>
+                </div>
+                <div className="AttributeContainer">
+                    <div className="AttributeName">Pattern</div>
+                    <div className="AttributeSelector"></div>
+                </div>
+
+                <div className="AttributeContainer">
+                    <div className="AttributeName">Style</div>
+                    <div className="AttributeSelector"></div>
+                </div>
+            </div>
+        );
     };
 
     return (
         <GenericYesNoPopup
-            title={'Label Info'}
+            title={`${labelRect.mode} Label Info`}
             renderContent={renderContent}
             acceptLabel={'Save'}
             onAccept={() => updateActivePopupTypeAction(null)}
