f0aafaca4e4cfff8e7db74ffa0a081d05ca31511
Piotr Skalski <piotr.skalski92@gmail.com>
2022-06-08 18:28:11 +0200

    feature/252_add_headers_for_exported_csv_file
    * ready for PR
    
    * small fix

--- src/logic/__tests__/export/RectLabelExport.test.ts
+++ src/logic/__tests__/export/RectLabelExport.test.ts
@@ -29,6 +29,7 @@ const labelNames: LabelName[] = [
         color: '#000000'
     },
 ]
+const imageName: string = 'image-name.png'
 
 describe('RectLabelsExporter wrapRectLabelIntoYOLO method', () => {
     it('should produce correct single label entry given issue #171 example 1', () => {
@@ -49,12 +50,18 @@ describe('RectLabelsExporter wrapRectLabelIntoYOLO method', () => {
         // when
         const result = RectLabelsExporter.wrapRectLabelIntoYOLO(labelRect, labelNames, imageSize)
         // then
-        const [classIdx, x, y, width, height] = result.split(' ').map((value: string) => parseFloat(value))
-        expect(classIdx).toBe(2)
-        expect(x + width / 2 <= 1).toBeTruthy()
-        expect(x - width / 2 >= 0).toBeTruthy()
-        expect(y + height / 2 <= 1).toBeTruthy()
-        expect(y - height / 2 >= 0).toBeTruthy()
+        const [
+            resultClassIdx,
+            resultX,
+            resultY,
+            resultWidth,
+            resultHeight
+        ] = result.split(' ').map((value: string) => parseFloat(value))
+        expect(resultClassIdx).toBe(2)
+        expect(resultX + resultWidth / 2 <= 1).toBeTruthy()
+        expect(resultX - resultWidth / 2 >= 0).toBeTruthy()
+        expect(resultY + resultHeight / 2 <= 1).toBeTruthy()
+        expect(resultY - resultHeight / 2 >= 0).toBeTruthy()
     })
 
     it('should produce correct single label entry given issue #171 example 2', () => {
@@ -75,12 +82,18 @@ describe('RectLabelsExporter wrapRectLabelIntoYOLO method', () => {
         // when
         const result = RectLabelsExporter.wrapRectLabelIntoYOLO(labelRect, labelNames, imageSize)
         // then
-        const [classIdx, x, y, width, height] = result.split(' ').map((value: string) => parseFloat(value))
-        expect(classIdx).toBe(2)
-        expect(x + width / 2 <= 1).toBeTruthy()
-        expect(x - width / 2 >= 0).toBeTruthy()
-        expect(y + height / 2 <= 1).toBeTruthy()
-        expect(y - height / 2 >= 0).toBeTruthy()
+        const [
+            resultClassIdx,
+            resultX,
+            resultY,
+            resultWidth,
+            resultHeight
+        ] = result.split(' ').map((value: string) => parseFloat(value))
+        expect(resultClassIdx).toBe(2)
+        expect(resultX + resultWidth / 2 <= 1).toBeTruthy()
+        expect(resultX - resultWidth / 2 >= 0).toBeTruthy()
+        expect(resultY + resultHeight / 2 <= 1).toBeTruthy()
+        expect(resultY - resultHeight / 2 >= 0).toBeTruthy()
     })
 
     it('should produce correct single label entry given issue #171 example 3', () => {
@@ -101,11 +114,57 @@ describe('RectLabelsExporter wrapRectLabelIntoYOLO method', () => {
         // when
         const result = RectLabelsExporter.wrapRectLabelIntoYOLO(labelRect, labelNames, imageSize)
         // then
-        const [classIdx, x, y, width, height] = result.split(' ').map((value: string) => parseFloat(value))
-        expect(classIdx).toBe(2)
-        expect(x + width / 2 <= 1).toBeTruthy()
-        expect(x - width / 2 >= 0).toBeTruthy()
-        expect(y + height / 2 <= 1).toBeTruthy()
-        expect(y - height / 2 >= 0).toBeTruthy()
+        const [
+            resultClassIdx,
+            resultX,
+            resultY,
+            resultWidth,
+            resultHeight
+        ] = result.split(' ').map((value: string) => parseFloat(value))
+        expect(resultClassIdx).toBe(2)
+        expect(resultX + resultWidth / 2 <= 1).toBeTruthy()
+        expect(resultX - resultWidth / 2 >= 0).toBeTruthy()
+        expect(resultY + resultHeight / 2 <= 1).toBeTruthy()
+        expect(resultY - resultHeight / 2 >= 0).toBeTruthy()
+    })
+})
+
+describe('RectLabelsExporter wrapRectLabelIntoCSV method', () => {
+    it('should produce correct single label', () => {
+        // given
+        const labelRect: LabelRect = {
+            id: 'label-rect-000',
+            labelId: 'label-002',
+            rect: {
+                x: 444,
+                y: 998,
+                width: 90,
+                height: 82
+            },
+            isCreatedByAI: false,
+            status: LabelStatus.ACCEPTED,
+            suggestedLabel: 'label-000'
+        }
+        // when
+        const result = RectLabelsExporter.wrapRectLabelIntoCSV(labelRect, labelNames, imageSize, imageName)
+        // then
+        const [
+            resultClassIdx,
+            resultBoxX,
+            resultBoxY,
+            resultBoxWidth,
+            resultBoxHeight,
+            resultImageName,
+            resultImageWidth,
+            resultImageHeight,
+        ] = result.split(',')
+        expect(resultClassIdx).toBe('label-name-002')
+        expect(parseFloat(resultBoxX)).toBe(444)
+        expect(parseFloat(resultBoxY)).toBe(998)
+        expect(parseFloat(resultBoxWidth)).toBe(90)
+        expect(parseFloat(resultBoxHeight)).toBe(82)
+        expect(resultImageName).toBe(imageName)
+        expect(parseFloat(resultImageWidth)).toBe(1920)
+        expect(parseFloat(resultImageHeight)).toBe(1080)
     })
 })
