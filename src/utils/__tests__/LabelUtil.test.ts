2f83f29a9235eed44b6b72041901b80a34c8c5b9
SkalskiP <piotr.skalski92@gmail.com>
2022-07-18 22:32:05 +0200

    more tests

--- src/utils/__tests__/LabelUtil.test.ts
+++ src/utils/__tests__/LabelUtil.test.ts
@@ -1,7 +1,8 @@
 import { IRect } from '../../interfaces/IRect';
 import { LabelUtil } from '../LabelUtil';
-import {LabelRect} from '../../store/labels/types';
+import {LabelPoint, LabelPolygon, LabelRect} from '../../store/labels/types';
 import {LabelStatus} from '../../data/enums/LabelStatus';
+import {IPoint} from '../../interfaces/IPoint';
 
 const mockUUID: string = '123e4567-e89b-12d3-a456-426614174000'
 
@@ -16,7 +17,7 @@ describe('LabelUtil createLabelRect method', () => {
             y: 100,
             width: 100,
             height: 100
-        }
+        };
 
         // when
         const result = LabelUtil.createLabelRect(labelId, rect);
@@ -26,6 +27,65 @@ describe('LabelUtil createLabelRect method', () => {
             id: mockUUID,
             labelId,
             rect,
+            isVisible: true,
+            isCreatedByAI: false,
+            status: LabelStatus.ACCEPTED,
+            suggestedLabel: null
+        }
+        expect(result).toEqual(expectedResult);
+    });
+});
+
+describe('LabelUtil createLabelPolygon method', () => {
+    it('return correct LabelPolygon object', () => {
+        // given
+        const labelId: string = '1';
+        const vertices: IPoint[] = [
+            {
+                x: 100,
+                y: 100
+            },
+            {
+                x: 100,
+                y: 200
+            },
+            {
+                x: 200,
+                y: 100
+            }
+        ];
+
+        // when
+        const result = LabelUtil.createLabelPolygon(labelId, vertices);
+
+        // then
+        const expectedResult: LabelPolygon = {
+            id: mockUUID,
+            labelId,
+            vertices,
+            isVisible: true
+        }
+        expect(result).toEqual(expectedResult);
+    });
+});
+
+describe('LabelUtil createLabelPoint method', () => {
+    it('return correct LabelPoint object', () => {
+        // given
+        const labelId: string = '1';
+        const point: IPoint = {
+            x: 100,
+            y: 100
+        };
+
+        // when
+        const result = LabelUtil.createLabelPoint(labelId, point);
+
+        // then
+        const expectedResult: LabelPoint = {
+            id: mockUUID,
+            labelId,
+            point,
             isVisible: true,
             isCreatedByAI: false,
             status: LabelStatus.ACCEPTED,
