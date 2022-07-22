0d319ba487a025a09b2e9bd57bb5e4dd0b4defd8
cuong <cuongseven789@gmail.com>
2022-07-22 16:26:32 +0700

    update code

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,15 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import configureStore from './configureStore';
-import {Provider} from 'react-redux';
-import {AppInitializer} from './logic/initializer/AppInitializer';
+import { Provider } from 'react-redux';
+import { AppInitializer } from './logic/initializer/AppInitializer';
 
 
 export const store = configureStore();
 AppInitializer.inti();
-
 ReactDOM.render(
     (<Provider store={store}>
-        <App/>
+        <App />
     </Provider>),
     document.getElementById('root') || document.createElement('div') // fix for testing purposes
 );
